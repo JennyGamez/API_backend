0ca98500e08be9e1c531eaf66f761671fa97b2e9
Jenny Gamez <jmgamez@hp14an-1.skinatech.com>
2019-05-13 15:31:13 -0500

    [Add] se cargan modulos nuevos

---
+++ api/components/HelperEncrypt.php
@@ -0,0 +1,32 @@
+<?php
+namespace api\components;
+
+/**
+ * Clase para encriptar parametros
+ */
+class HelperEncrypt{
+    public function encrypt($string){
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'llaveEncriptacion';
+        $secret_iv = 'vectorInicializacion';
+        $key = hash('sha256', $secret_key);
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+        $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+        $output = base64_encode($output);
+        return $output;
+    }
+
+    public function decrypt($string){
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'llaveEncriptacion';
+        $secret_iv = 'vectorInicializacion';
+        $key = hash('sha256', $secret_key);
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+        $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        return $output;
+    }
+}
